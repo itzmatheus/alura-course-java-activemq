3536c855725d63da35587e490d271dbd9db10730
Matheus Leite <matheusjoselfm@icloud.com>
2022-11-11 21:13:07 -0300

    add producer

---
+++ src/main/java/Producer.java
@@ -0,0 +1,33 @@
+import javax.jms.*;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.Scanner;
+
+public class Producer {
+
+    public static void main(String[] args) throws NamingException, JMSException {
+
+        var context = new InitialContext();
+        ConnectionFactory connectionFactory = (ConnectionFactory) context.lookup("ConnectionFactory");
+        Connection connection = connectionFactory.createConnection();
+        connection.start();
+
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        Destination fila = (Destination) context.lookup("filaFinanceiro");
+
+        MessageProducer producer = session.createProducer(fila);
+
+
+        for (int i = 0; i < 1000; i++) {
+            Message message = session.createTextMessage("MSG ID: " + i);
+            producer.send(message);
+        }
+
+        session.close();
+        connection.close();
+        context.close();
+
+    }
+
+}
