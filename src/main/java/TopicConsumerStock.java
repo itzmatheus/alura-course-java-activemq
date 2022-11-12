1107ef42750f8acf20b934466e56d8118ed7c77c
Matheus Leite <matheusjoselfm@icloud.com>
2022-11-11 21:57:52 -0300

    add two consumers

---
+++ src/main/java/TopicConsumerStock.java
@@ -0,0 +1,38 @@
+import javax.jms.*;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.Scanner;
+
+public class TopicConsumerStock {
+
+    public static void main(String[] args) throws NamingException, JMSException {
+
+        var context = new InitialContext();
+        ConnectionFactory connectionFactory = (ConnectionFactory) context.lookup("ConnectionFactory");
+        Connection connection = connectionFactory.createConnection();
+        connection.setClientID("stock");
+
+        connection.start();
+
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        Topic topic = (Topic) context.lookup("topicoLoja");
+        MessageConsumer consumer = session.createDurableSubscriber(topic, "subscriber");
+
+        consumer.setMessageListener(message -> {
+            TextMessage textMessage = (TextMessage) message;
+            try {
+                System.out.println(textMessage.getText());
+            } catch (JMSException e) {
+                System.out.println(message);
+            }
+        });
+        new Scanner(System.in).nextLine();
+
+        session.close();
+        connection.close();
+        context.close();
+
+    }
+
+}
